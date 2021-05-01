074da5cb53ef8e3e124af5af5b62f4c8b3ea6afa
adagajic <adagajic@gmail.com>
2021-05-01 13:54:51 +0200

    prints board

--- tabla.py
+++ tabla.py
@@ -1,5 +1,5 @@
 import pygame
-from .figure import Figura
+from figure import Figura
 pygame.init()
 WHITE = (255,255,255)
 GREEN = (0,255,0)
@@ -8,7 +8,7 @@ BLACK = (0,0,0)
 size = (800,800)
 class Tabla:
     def __init__(self):
-        self.board = [][]
+        self.board = [0]*64
     def drawBoard(self,screen):
         for row in range(8):
             for column in range(8):
@@ -17,11 +17,11 @@ class Tabla:
                 else:
                     pygame.draw.rect(screen,GREEN,[column*100,row*100,100,100])
                     if row<3:
-                        self.board[row][column] = Figura(row,column,WHITE)
-                        fig = self.board[row][column]
+                        self.board[row*8+column] = Figura(row,column,WHITE)
+                        fig = self.board[row*8+column]
                         fig.drawcircle(screen)
-                    if row>4
-                        self.board[row][column] = Figura(row,column,BLACK)
-                        fig = self.board[row][column]
+                    if row>4:
+                        self.board[row*8+column] = Figura(row,column,BLACK)
+                        fig = self.board[row*8+column]
                         fig.drawcircle(screen)
 
